3b584189ac6c4cd8830aace09627aa9b1e33fefa
yangguoqiang01 <wochange@163.com>
2019-01-15 16:11:30 +0800

    add axios

--- Ajax/axiosInitHelper.js
+++ Ajax/axiosInitHelper.js
@@ -24,12 +24,18 @@ const tryCloseLoading = () => {
   }
 }
 
+// 那么showFullScreenLoading tryHideFullScreenLoading()要干的事儿就是将同一时刻的请求合并。声明一个变量needLoadingRequestCount，每次调用showFullScreenLoading方法 needLoadingRequestCount + 1。调用tryHideFullScreenLoading()方法，needLoadingRequestCount - 1。needLoadingRequestCount为 0 时，结束 loading。
+
 export function showFullScreenLoading() {
   if (needLoadingRequestCount === 0) {
     startLoading()
   }
   needLoadingRequestCount++
 }
+
+
+// 在 request1 的 loading 还没结束时，request2 的 loading 已经开始。这种情况 request1 和 request2 在时间上有一定的重合，所以 loading 可以合并。
+// 那么 request3 是在 request2 结束后 100ms 开始 loading.这时你会发现 loading 两次，并且中间有一次极短的闪烁，这当然是很不好的体验了。
 
 export function tryHideFullScreenLoading() {
   if (needLoadingRequestCount <= 0) return
--- Ajax/axios_loading.js
+++ Ajax/axios_loading.js
@@ -1,3 +1,6 @@
+// 首先，请求开始的时候开始 loading，
+// 然后在请求返回后结束 loading。重点就是要拦截请求和响应。
+// 然后，要解决多个请求合并为一次 loading。
 import axios from 'axios'
 
 import env from '@/../config/prod.env'
@@ -16,6 +19,9 @@ const $ = axios.create({
 })
 
 // 请求拦截器
+// axios 提供了请求拦截和响应拦截的接口，
+// 每次请求都会调用showFullScreenLoading方法，
+// 每次响应都会调用tryHideFullScreenLoading()方法
 $.interceptors.request.use((config) => {
   if (config.showLoading) {
     showFullScreenLoading()
@@ -35,6 +41,7 @@ $.interceptors.response.use((response) => {
   tryHideFullScreenLoading()
   return Promise.reject(error)
 })
+// 实际上，现在的功能还差一点。如果某个请求不需要 loading 呢，那么发请求的时候加个 showLoading： false的参数就好了。在请求拦截和响应拦截时判断下该请求是否需要loading，需要 loading 再去调用showFullScreenLoading()方法即可。
 
 const defaultConfig = { showLoading: true }
 export default {
